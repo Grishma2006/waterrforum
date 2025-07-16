4633ade6cc04878cd8401cd74c2e3f5b6adeafc4
Milaap Barhate <grishma.barhate@gmail.com>
2025-07-16 08:21:13 +0530

    Updated layout and CSS for mobile responsiveness

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
   <title>Home - Water Forum</title>
   <link rel="stylesheet" href="style.css">
 </head>
